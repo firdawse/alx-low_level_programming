2907f9da1c2e52f138fc38448291fa5b00c15794
firdawse <firdawse.guerbouzi1@gmail.com>
2023-04-23 06:08:22 -0700

    tasks

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void) {
+    int i, j, k;
+    for (i = 0; i < 7; i++) {
+        for (j = i + 1; j < 8; j++) {
+            for (k = j + 1; k < 9; k++) {
+                putchar(i%3 + '0');
+                putchar(j%3 + '0');
+                putchar(k%3 + '0');
+                if (i != 6 || j != 7 || k != 8) {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    return 0;
+}
