790b3a5c3ba76f6a1f4f21c5109be2062c5df29f
firdawse <firdawse.guerbouzi1@gmail.com>
2023-05-02 12:54:52 -0700

    tasks

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,8 @@
 /**
  * function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
- * Return  n
+ * @n : input
+ * Return:  n
  */
 void reset_to_98(int *n)
 {
