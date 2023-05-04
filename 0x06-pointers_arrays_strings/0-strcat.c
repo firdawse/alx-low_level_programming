9494af1dd8dc10eb9058d324a3e865cfce4af0ad
firdawse <firdawse.guerbouzi1@gmail.com>
2023-05-04 06:47:28 -0700

    tasks

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,13 +3,13 @@
                                                                                                                                         
 /**                                                                                                                                     
  * _putchar - writes the character c to stdout                                                                                          
- * @s: The character to print                                                                                                           
+ * @c: The character to print                                                                                                           
  *                                                                                                                                      
  * Return: On success 1.                                                                                                                
  * On error, -1 is returned, and errno is set appropriately.                                                                            
  */                                                                                                                                     
-int _putchar(char s)                                                                                                                    
+int _putchar(char c)                                                                                                                    
 {                                                                                                                                       
-        return (write(1, &s, 1));                                                                                                       
+        return (write(1, &c, 1));                                                                                                       
 }
-
+                                   
