287327f10c3f5f06daa45f9130dc4591a0e5542d
firdawse <firdawse.guerbouzi1@gmail.com>
2023-05-08 13:21:26 -0700

    tasks

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_chessboard - Prints the chessboard.
+ * @board: The 2D array representing the chessboard.
+ */
+void print_chessboard(char (*board)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			putchar(board[row][col]);
+		}
+		putchar('\n');
+	}
+}
