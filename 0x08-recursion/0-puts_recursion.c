6dca5a077f9d8678222c73fdce0ee881b926e0ad
firdawse <firdawse.guerbouzi1@gmail.com>
2023-05-10 10:31:02 -0700

    tasks

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,13 @@
 #include "main.h"
+#include <unistd.h>
 /**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-void _puts_recursion(char *s)
+int _putchar(char c)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-
-	else
-		_putchar('\n');
+	return (write(1, &c, 1));
 }
