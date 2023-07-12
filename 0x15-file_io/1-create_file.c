7865530e9dcd4ec1c53676aab9519ae822681856
firdawse <firdawse.guerbouzi1@gmail.com>
2023-07-12 03:55:44 -0700

    changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[length]; length++;
+		for (length = 0; text_content[length]; length++;
 	}
 
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
