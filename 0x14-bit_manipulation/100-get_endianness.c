3dcff1adb6cac225087858ef88b4217f2443d61e
firdawse <firdawse.guerbouzi1@gmail.com>
2023-07-12 09:18:55 -0700

    task6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian 1 otherwise
+ */
+int get_endianness(void)
+{
+	unsigned int i;
+	char *s;
+
+
+	i = 1;
+	s = (char *) &i;
+
+	return ((int)*s);
+}
