022773335cae93825da441c558dc63184acab049
firdawse <firdawse.guerbouzi1@gmail.com>
2023-07-27 17:14:37 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,33 @@
-#include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
-
+#include <fcntl.h>   
+#include <sys/stat.h> 
+#include <unistd.h> 
+#include <string.h>
 /**
- * create_file - Creates a file.
- * @filename: Pointer to the name of the file to create.
- * @text_content: Pointer to the content to write into the file.
+ * create_file - create a file and write into it 
+ * @filename: the name of the file
+ * @text_content: the data to write 
  *
- * Return: 1 on success, -1 on failure.
+ * Return:  1 if success Otherwise -1.
  */
 int create_file(const char *filename, char *text_content)
 {
-        int file, n, length = 0;
+	ssize_t data = 0;
+	int fd;
 
-        if (filename == NULL)
-                return (-1);
+	if (!filename)
+		return (-1);
 
-        if (text_content != NULL)
-        {
-                for (length = 0; text_content[length]; length++);
-        }
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-        file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-        if (file == -1)
-                return (-1);
+	if (fd < 0)
+		return (-1);
 
-        n = write(file, text_content, length);
-        if (n == -1)
-        {
-                close(file);
-                return (-1);
-        }
+	if (text_content)
+		data = write(fd, text_content, strlen(text_content));
 
-        close(file);
-        return (1);
+	close(fd);
+
+	if (data < 0)
+		return (-1);
+	return (1);
 }
-
