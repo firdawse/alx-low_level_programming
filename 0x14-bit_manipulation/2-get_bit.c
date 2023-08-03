9f1365cf27bca02eea75f024ab73b5f26adef6f0
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-03 11:38:56 -0700

    task2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,19 +10,12 @@
  
 int get_bit(unsigned long int num, unsigned int ind)
 {
-	unsigned int i;
-
-	if (num == 0 && ind < 64)
-		return (0);
-
-	for (i = 0; i <= 63; num >>= 1, i++)
+	if (ind >= sizeof(unsigned long int) * 8)
 	{
-		if (ind == i)
-		{
-			return (num & 1);
-		}
+		
+		return (-1);
 	}
 
-	return (-1);
+	unsigned long int mask = 1UL << ind;
+	return ((num & mask) ? 1 : 0); 
 }
-
