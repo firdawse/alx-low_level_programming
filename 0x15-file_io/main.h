4e14b406f1efa4d4d9c6c5f50456d6e5200705a0
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-07 07:41:13 -0700

    task4

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,17 @@
+#include <elf.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _strncmp(const char *s1, const char *s2, size_t n);
+void elf_close(int fd);
+void file_read(int fd, char *buf, size_t count);
+void magic(const unsigned char *buffer);
+size_t class(const unsigned char *buffer);
+int data(const unsigned char *buffer);
+void version(const unsigned char *buffer);
+void osabi(const unsigned char *buffer);
+void abi(const unsigned char *buffer);
+void type(const unsigned char *buffer, int needed);
+void entry(const unsigned char *buffer, size_t bit_mode, int needed);
