09a889d48795ab4687a44d50e0fc7f22d65cab41
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-07 07:07:40 -0700

    last

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -20,4 +20,14 @@ void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 int create_file(const char *filename, char *text_content);
+int _strncmp(const char *s1, const char *s2, size_t n);
+void elf_close(int fd);
+void magic(const unsigned char *buffer);
+size_t class(const unsigned char *buffer);
+int data(const unsigned char *buffer);
+void version(const unsigned char *buffer);
+void osabi(const unsigned char *buffer);
+void abi(const unsigned char *buffer);
+void type(const unsigned char *buffer, int needed);
+void entry(const unsigned char *buffer, size_t bit_mode, int needed);
 
