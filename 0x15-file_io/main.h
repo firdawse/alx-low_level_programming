6d9c96bf4b3c03cdb7b58f662653949a0ff9531e
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-07 06:59:21 -0700

    main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int append_text_to_file(const char *filename, char *text_content);
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
