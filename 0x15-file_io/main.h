6ed7fa10eb6e5e4634e4cd1bc0051033f31a02ea
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-07 07:59:39 -0700

    task1

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,14 @@
-#include <elf.h>
+#ifndef MAIN
+#define MAIN
+
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *file_name, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 int _strncmp(const char *s1, const char *s2, size_t n);
 void elf_close(int fd);
 void file_read(int fd, char *buf, size_t count);
@@ -16,3 +20,5 @@ void osabi(const unsigned char *buffer);
 void abi(const unsigned char *buffer);
 void type(const unsigned char *buffer, int needed);
 void entry(const unsigned char *buffer, size_t bit_mode, int needed);
+
+#endif
