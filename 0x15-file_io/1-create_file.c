450e14a1fde932bb7f3760b25434d59ed40efd00
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-07 08:15:45 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,24 +13,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t data = 0;
-	int fd;
+	int o, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (text_content != NULL)
+	{
+		length = strlen(text_content);
+	}
 
-	if (fd < 0)
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, length);
+
+	if (o == -1 || w == -1)
 		return (-1);
 
-	if (text_content)
-		data = write(fd, text_content, strlen(text_content));
+	close(o);
 
-	close(fd);
-
-	if (data < 0)
-		return (-1);
 	return (1);
 }
 
