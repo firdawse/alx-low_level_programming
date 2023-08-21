3a55c72e064426a32322cbc509ec7864b1ba0a68
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-21 16:44:25 -0700

    task0

---
+++ 0x18-dynamic_libraries/libdynamic_functions.c
@@ -0,0 +1,238 @@
+#include "main.h"
+
+/**
+ * _putchar - Writes a character to stdout.
+ * @c: The character to write.
+ * Return: On success, 1. On error, -1.
+ */
+int _putchar(char c)
+{
+    /* Dummy implementation */
+    return 0;
+}
+
+/**
+ * _islower - Checks if a character is lowercase.
+ * @c: The character to check.
+ * Return: 1 if c is lowercase, 0 otherwise.
+ */
+int _islower(int c)
+{
+    /* Dummy implementation */
+    return 0;
+}
+
+/**
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to check.
+ * Return: 1 if c is alphabetic, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+    /* Dummy implementation */
+    return 0;
+}
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer.
+ * Return: The absolute value of n.
+ */
+int _abs(int n)
+{
+    /* Dummy implementation */
+    return 0;
+}
+
+/**
+ * _isupper - Checks if a character is uppercase.
+ * @c: The character to check.
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+    /* Dummy implementation */
+    return 0;
+}
+
+/**
+ * _isdigit - Checks if a character is a digit.
+ * @c: The character to check.
+ * Return: 1 if c is a digit, 0 otherwise.
+ */
+int _isdigit(int c)
+{
+    /* Dummy implementation */
+    return 0;
+}
+
+/**
+ * _strlen - Computes the length of a string.
+ * @s: The string.
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+    /* Dummy implementation */
+    return 0;
+}
+
+/**
+ * _puts - Writes a string to stdout followed by a new line.
+ * @s: The string to write.
+ */
+void _puts(char *s)
+{
+    /* Dummy implementation */
+}
+
+/**
+ * _strcpy - Copies a string from src to dest.
+ * @dest: The destination buffer.
+ * @src: The source string.
+ * Return: The pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+    /* Dummy implementation */
+    return dest;
+}
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to convert.
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+    /* Dummy implementation */
+    return 0;
+}
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ * Return: The pointer to dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+    /* Dummy implementation */
+    return dest;
+}
+
+/**
+ * _strncat - Concatenates n bytes of src to dest.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The number of bytes to concatenate.
+ * Return: The pointer to dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+    /* Dummy implementation */
+    return dest;
+}
+
+/**
+ * _strncpy - Copies n bytes of src to dest.
+ * @dest: The destination buffer.
+ * @src: The source string.
+ * @n: The number of bytes to copy.
+ * Return: The pointer to dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+    /* Dummy implementation */
+    return dest;
+}
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ * Return: 0 if strings are equal, negative if s1 < s2, positive if s1 > s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+    /* Dummy implementation */
+    return 0;
+}
+
+/**
+ * _memset - Fills a block of memory with a value.
+ * @s: The memory block.
+ * @b: The value to set.
+ * @n: The number of bytes to set.
+ * Return: The pointer to the memory block.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+    /* Dummy implementation */
+    return s;
+}
+
+/**
+ * _memcpy - Copies n bytes from src to dest.
+ * @dest: The destination buffer.
+ * @src: The source buffer.
+ * @n: The number of bytes to copy.
+ * Return: The pointer to dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    /* Dummy implementation */
+    return dest;
+}
+
+/**
+ * _strchr - Locates the first occurrence of a character in a string.
+ * @s: The string to search.
+ * @c: The character to find.
+ * Return: A pointer to the first occurrence of c in s, or NULL if not found.
+ */
+char *_strchr(char *s, char c)
+{
+    /* Dummy implementation */
+    return NULL;
+}
+
+/**
+ * _strspn - Computes the length of the initial segment of s that consists
+ *           entirely of characters from accept.
+ * @s: The string to search.
+ * @accept: The characters to match.
+ * Return: The length of the initial segment.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+    /* Dummy implementation */
+    return 0;
+}
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: The string to search.
+ * @accept: The characters to search for.
+ * Return: A pointer to the first occurrence of a character from accept in s,
+ *         or NULL if not found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+    /* Dummy implementation */
+    return NULL;
+}
+
+/**
+ * _strstr - Locates a substring within a string.
+ * @haystack: The string to search.
+ * @needle: The substring to find.
+ * Return: A pointer to the first occurrence of the substring in haystack,
+ *         or NULL if not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+    /* Dummy implementation */
+    return NULL;
+}
+
